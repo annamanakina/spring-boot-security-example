3885dda3445d5f6cfaced540d3e40c875df3ecc2
Anna Manakina <annamanakina@gmail.com>
2019-04-30 11:48:09 +0300

    small refactor of Auditorium entity

--- src/main/java/com/example/spring/boot/security/springbootdemosecurity/model/Auditorium.java
+++ src/main/java/com/example/spring/boot/security/springbootdemosecurity/model/Auditorium.java
@@ -1,49 +1,18 @@
 package com.example.spring.boot.security.springbootdemosecurity.model;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.stereotype.Component;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-
 
 @Entity
 @Setter
 @Getter
-//@NoArgsConstructor
-@Component
 public class Auditorium {
 
     @Id
     private Integer id;
     private String title;
     private Integer numberOfSeats;
-
-    //or @Value("${maxVal}")
-    /*@Value("#{'${auditorium.vipSeats}'.split(',')}")
-    private Set<Integer> vipSeats;
-
-    public Set<Integer> getAllSeats() {
-        return IntStream.range(1, numberOfSeats+1).boxed().collect(Collectors.toSet());
-    }
-
-    public Set<Integer> getVipSeats() {
-        return vipSeats;
-    }
-
-    public long countVipSeats(Collection<Integer> seats) {
-        return seats.stream().filter(seat -> vipSeats.contains(seat)).count();
-    }
-
-    @Override
-    public String toString() {
-        final StringBuilder sb = new StringBuilder("Auditorium{");
-        sb.append("id=").append(id);
-        sb.append(", title='").append(title).append('\'');
-        sb.append(", numberOfSeats=").append(numberOfSeats);
-        sb.append(", vipSeats=").append(vipSeats);
-        sb.append('}');
-        return sb.toString();
-    }*/
+    private String vipSeats;
 }
